f62551b186ddecfcdb9d5b05cca5f47d0dc2501c
Minguk Gaang <minguk.gaang@gmail.com>
2023-05-24 18:34:01 +0200

    first try

---
+++ src/conjunctive_normal_form.rs
@@ -0,0 +1,6 @@
+mod negation_normal_form;
+use negation_normal_form::apply_nnf;
+
+fn conjunctive_normal_form(formula: &str) -> String {
+
+}
--- src/negation_normal_form.rs
+++ src/negation_normal_form.rs
@@ -1,76 +1,61 @@
-struct binary_node {
+#[derive(Clone)]
+struct BinaryNode {
     value: String,
-    left: Option<Box<ast_node>>,
-    right: Option<Box<ast_node>>
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>
 }
 
-struct unary_node {
+#[derive(Clone)]
+struct UnaryNode {
     value: String,
-    child: Option<Box<ast_node>>
+    child: Option<Box<Node>>
 }
 
+#[derive(Clone)]
+struct LeafNode {
+    value: String
+}
+
+#[derive(Clone)]
 enum Node {
-    Binary(binary_node),
-    Unary(unary_node),
-    Leaf(String)
+    Binary(BinaryNode),
+    Unary(UnaryNode),
+    Leaf(LeafNode)
 }
 
-impl Node {
-    fn is_leaf(&self) -> bool {
-        match self {
-            Node::Leaf(_) => true,
-            _ => false
-        }
-    }
-
-    fn is_binary(&self) -> bool {
-        match self {
-            Node::Binary(_) => true,
-            _ => false
-        }
-    }
-
-    fn is_unary(&self) -> bool {
-        match self {
-            Node::Unary(_) => true,
-            _ => false
-        }
-    }
-}
-
-fn get_not_node(node: Node) -> Node {
-    return Node::Unary(unary_node {
+fn get_not_node(node: &Node) -> Node {
+    return Node::Unary(UnaryNode {
         value: "!".to_string(),
-        child: Some(Box::new(node))
+        child: Some(Box::new(node.clone()))
     });
 }
 
-fn get_and_node(lhs: Node, rhs: Node) -> Node {
-    return Node::Binary(binary_node {
+fn get_and_node(lhs: &Node, rhs: &Node) -> Node {
+    return Node::Binary(BinaryNode {
         value: "&".to_string(),
-        left: Some(Box::new(lhs)),
-        right: Some(Box::new(rhs))
+        left: Some(Box::new(lhs.clone())),
+        right: Some(Box::new(rhs.clone()))
     });
 }
 
-fn get_or_node(lhs: Node, rhs: Node) -> Node {
-    return Node::Binary(binary_node {
+fn get_or_node(lhs: &Node, rhs: &Node) -> Node {
+    return Node::Binary(BinaryNode {
         value: "|".to_string(),
-        left: Some(Box::new(lhs)),
-        right: Some(Box::new(rhs))
+        left: Some(Box::new(lhs.clone())),
+        right: Some(Box::new(rhs.clone()))
     });
 }
 
-fn str_to_ast(formula: &str) -> ast_node {
+fn str_to_ast(formula: &str) -> Node {
     let mut stack = Vec::new();
 
     for c in formula.chars() {
         match c {
-            c.is_uppercase() => stack.push(Node::Leaf(c.to_string())),
+            _ if c.is_uppercase() => stack.push(Node::Leaf(LeafNode { value: c.to_string() })),
             '&' | '|' => {
                 let rhs = stack.pop().expect("Invalid syntax");
                 let lhs = stack.pop().expect("Invalid syntax");
-                stack.push(Node::Binary(binary_node {
+                stack.push(Node::Binary(BinaryNode {
                     value: c.to_string(),
                     left: Some(Box::new(lhs)), 
                     right: Some(Box::new(rhs))
@@ -79,30 +64,30 @@ fn str_to_ast(formula: &str) -> ast_node {
             '^' => {
                 let rhs = stack.pop().expect("Invalid syntax");
                 let lhs = stack.pop().expect("Invalid syntax");
-                let not_rhs = get_not_node(rhs);
-                let not_lhs = get_not_node(lhs);
-                let lhs_and_not_rhs = get_and_node(lhs, not_rhs);
-                let not_lhs_and_rhs = get_and_node(not_lhs, rhs);
-                stack.push(get_or_node(lhs_and_not_rhs, not_lhs_and_rhs));
+                let not_rhs = get_not_node(&rhs);
+                let not_lhs = get_not_node(&lhs);
+                let lhs_and_not_rhs = get_and_node(&lhs, &not_rhs);
+                let not_lhs_and_rhs = get_and_node(&not_lhs, &rhs);
+                stack.push(get_or_node(&lhs_and_not_rhs, &not_lhs_and_rhs));
             },
             '>' => {
                 let rhs = stack.pop().expect("Invalid syntax");
                 let lhs = stack.pop().expect("Invalid syntax");
-                let not_lhs = get_not_node(lhs);
-                stack.push(get_or_node(not_lhs, rhs));
+                let not_lhs = get_not_node(&lhs);
+                stack.push(get_or_node(&not_lhs, &rhs));
             },
             '=' => {
                 let rhs = stack.pop().expect("Invalid syntax");
                 let lhs = stack.pop().expect("Invalid syntax");
-                let not_rhs = get_not_node(rhs);
-                let not_lhs = get_not_node(lhs);
-                let lhs_and_rhs = get_and_node(lhs, rhs);
-                let not_lhs_and_not_rhs = get_and_node(not_lhs, not_rhs);
-                stack.push(get_or_node(lhs_and_rhs, not_lhs_and_not_rhs));
+                let not_rhs = get_not_node(&rhs);
+                let not_lhs = get_not_node(&lhs);
+                let lhs_and_rhs = get_and_node(&lhs, &rhs);
+                let not_lhs_and_not_rhs = get_and_node(&not_lhs, &not_rhs);
+                stack.push(get_or_node(&lhs_and_rhs, &not_lhs_and_not_rhs));
             },
             '!' => {
                 let val = stack.pop().expect("Invalid syntax");
-                stack.push(Node::Unary(unary_node {
+                stack.push(Node::Unary(UnaryNode {
                     value: c.to_string(),
                     child: Some(Box::new(val))
                 }));
@@ -113,54 +98,66 @@ fn str_to_ast(formula: &str) -> ast_node {
     if stack.len() != 1 {
         panic!("Invalid syntax");
     }
-    stack.pop.expect("Invalid syntax");
+    let root = stack.pop().expect("Invalid syntax");
+    return root;
 }
 
 fn apply_nnf(curr: Node) -> Node {
-    if curr.is_leaf() {
-        return curr;
-    } else if curr.is_unary() {
-        if curr.child.is_leaf() {
-            return curr;
-        } else {
-            let child = apply_nnf(curr.child);
-            if child.is_leaf() {
-                return get_not_node(child);
-            } else if child.is_unary() {
-                return child.child;
-            } else if child.is_binary() {
-                let lhs = apply_nnf(child.left);
-                let rhs = apply_nnf(child.right);
-                if child.value == "&" {
-                    return get_or_node(get_not_node(lhs), get_not_node(rhs));
-                } else if child.value == "|" {
-                    return get_and_node(get_not_node(lhs), get_not_node(rhs));
-                } else {
-                    unreachable!();
+    match curr {
+        Node::Leaf(leaf) => {
+            Node::Leaf(leaf)
+        },
+        Node::Binary(binary) => {
+            let left = binary.left.map(|box_node| apply_nnf(*box_node));
+            let right = binary.right.map(|box_node| apply_nnf(*box_node));
+            Node::Binary(BinaryNode {
+                value: binary.value,
+                left: left.map(Box::new),
+                right: right.map(Box::new),
+            })
+        },
+        Node::Unary(unary) => {
+            let child = unary.child.expect("Invalid syntax");
+            match *child {
+                Node::Binary(binary) => {
+                    let lhs = apply_nnf(*binary.left.expect("Invalid syntax"));
+                    let rhs = apply_nnf(*binary.right.expect("Invalid syntax"));
+                    if binary.value == "&" {
+                        return get_or_node(&get_not_node(&lhs), &get_not_node(&rhs));
+                    } else if binary.value == "|" {
+                        return get_and_node(&get_not_node(&lhs), &get_not_node(&rhs));
+                    } else {
+                        unreachable!();
+                    }
+                },
+                Node::Unary(unary_child) => {
+                    return apply_nnf(*unary_child.child.expect("Invalid syntax"));
+                },
+                _ => {
+                    return get_not_node(&apply_nnf(*child));
                 }
             }
-        }
-    } else {
-        let lhs = apply_nnf(curr.left);
-        let rhs = apply_nnf(curr.right);
-        if curr.value == "&" {
-            return get_and_node(lhs, rhs);
-        } else if curr.value == "|" {
-            return get_or_node(lhs, rhs);
-        } else {
-            unreachable!();
         }
     }
 }
 
-fn ast_to_str(ast: Node) -> String {
-    if ast.is_leaf() {
-        return ast.value;
-    } else if ast.is_unary() {
-        return ast_to_str(ast.child) + ast.value;
-    } else if ast.is_binary() {
-        return ast_to_str(ast.left) + ast_to_str(ast.right) + ast.value;
+fn ast_to_str(curr: Node) -> String {
+    let mut res: String = "".to_string();
+    match curr {
+        Node::Leaf(leaf) => {
+            res += &leaf.value;
+        },
+        Node::Binary(binary) => {
+            res += &binary.value;
+            res += &ast_to_str(*binary.right.expect("Invalid syntax"));
+            res += &ast_to_str(*binary.left.expect("Invalid syntax"));
+        },
+        Node::Unary(unary) => {
+            res += &ast_to_str(*unary.child.expect("Invalid syntax"));
+            res += &unary.value;
+        }
     }
+    return res.chars().rev().collect::<String>();
 }
 
 fn negation_normal_form(formula: &str) -> String {
@@ -171,7 +168,9 @@ fn negation_normal_form(formula: &str) -> String {
 }
 
 fn main() {
-    let formula = "A>B&C^DvE";
-    let res = negation_normal_form(formula);
-    println!("{}", res);
+    println!("{}", negation_normal_form("AB&!")); // A!B!|
+    println!("{}", negation_normal_form("AB|!")); // A!B!&
+    println!("{}", negation_normal_form("AB>")); // A!B|
+    println!("{}", negation_normal_form("AB=")); // AB&A!B!&|
+    println!("{}", negation_normal_form("AB|C&!")); // A!B!&C!|
 }
